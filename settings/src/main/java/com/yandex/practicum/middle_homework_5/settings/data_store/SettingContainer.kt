001bfe8430c8c9de8b881c2a4bb17d101798c10c
lbonifatsiy <lev.bonifatsiy@yandex.ru>
2025-09-16 12:58:21 +0300

    move settings to settings module

---
+++ settings/src/main/java/com/yandex/practicum/middle_homework_5/settings/data_store/SettingContainer.kt
@@ -0,0 +1,16 @@
+package com.yandex.practicum.middle_homework_5.settings.data_store
+
+data class SettingContainer(
+    val periodic: Long,
+    val delayed: Long,
+) {
+    companion object {
+        const val DEFAULT_REFRESH_PERIOD = 15L
+        const val FIST_LAUNCH_DELAY = 10L
+
+        val initial: SettingContainer = SettingContainer(
+            periodic = DEFAULT_REFRESH_PERIOD,
+            delayed = FIST_LAUNCH_DELAY
+        )
+    }
+}
