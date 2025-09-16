6545a86fbb458c2824e5563223d009aa15e23022
lbonifatsiy <lev.bonifatsiy@yandex.ru>
2025-09-16 12:21:47 +0300

    add settings module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 }
 
 dependencies {
+    implementation(project(":settings"))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
